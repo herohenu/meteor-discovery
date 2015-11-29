e242dbcef1325ef8807333a894f3709fff6a4853
Yinghao Lei <leiyinghao.bj@gmail.com>
2015-11-29 09:09:42 +0800

    7-6 使用内置方法提交帖子

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,7 +6,12 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title:$(e.target).find('[name=title]').val()
     };
-    post._id = Posts.insert(post);
-    Router.go('postPage',post);
+
+    Meteor.call('postInsert',post, function (err,result) {
+      if(err){
+        return alert(err.reason);
+      }
+      Router.go('postPage',{_id:result._id});
+    });
   }
 });
