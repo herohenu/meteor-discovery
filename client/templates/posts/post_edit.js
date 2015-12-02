0f9152b372cca650af6606977f57a231bffaae66
Yinghao Lei <leiyinghao.bj@gmail.com>
2015-12-02 19:52:46 +0800

    add edit and  js

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,24 @@
+Template.postEdit.events({
+	'submit form': function (e) {
+		e.preventDefault();
+		var currentPostId = this._id;
+		var postProperties = {
+			url:$(e.target).find('[name=url]').val(),
+			title:$(e.target).find('[name=title]').val()
+		}
+		Posts.update(currentPostId,{$set:postProperties}, function (err) {
+			if(err)
+			  alert(err.reason)
+			else
+			  Router.go('postsList',{_id:currentPostId});
+		});
+	},
+	'click .delete': function (e) {
+		e.preventDefault();
+		if(confirm("Deleted this post?"))
+		var currentPostId = this._id;
+		Posts.remove(currentPostId);
+		Router.go('postsList');
+	}
+
+})
